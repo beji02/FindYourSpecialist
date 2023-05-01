5859c1d5b612754989aacd9eab26d008947ce137
victorzarzu <victorzarzu@gmail.com>
2023-05-02 00:53:47 +0300

    repo

---
+++ FYS-persistance/src/main/java/fys/Main.java
@@ -0,0 +1,21 @@
+package fys;
+
+import fys.fysmodel.User;
+import fys.fyspersistance.users.UsersRepository;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("hibernate.cfg.xml");
+
+        UsersRepository usersRepository = context.getBean(UsersRepository.class);
+
+        User user = new User();
+        user.setFirstName("test first name");
+        user.setLastName("test last name");
+        user.setUsername("test username");
+        user.setPassword("test password");
+
+        usersRepository.add(user);
+    }
+}
