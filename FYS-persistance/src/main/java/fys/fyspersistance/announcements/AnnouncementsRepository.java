f19a09a0afe7401d66a9c0f8fb2446ebd600bbcc
victorzarzu <victorzarzu@gmail.com>
2023-05-02 00:59:29 +0300

    model

--- FYS-persistance/src/main/java/fys/fyspersistance/announcements/AnnouncementsRepository.java
+++
@@ -1,7 +0,0 @@
-package fys.fyspersistance.announcements;
-
-import fys.fysmodel.Announcement;
-import fys.fyspersistance.Repository;
-
-public interface AnnouncementsRepository extends Repository<Integer, Announcement> {
-}
