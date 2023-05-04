01cbffc883d93ddd511ea3f090fd73edc96a960c
DianaElenaStancu <stanciududa2002@gmail.com>
2023-05-04 22:20:27 +0300

    Create AnnouncementList and AnnouncementCard; logo.svg removed because we don't need it anymore

--- client/src/Home.js
+++ client/src/Home.js
@@ -2,10 +2,33 @@ import NavbarHome from "./NavbarHome";
 import Container from "react-bootstrap/Container";
 import React from "react";
 import FooterHome from "./FooterHome";
+import AnnouncementList from "./AnnouncementList";
 function Home() {
+    const announcements = [
+        {
+            id: 1,
+            title: "Announcement 1",
+            description: "This is the first announcement",
+            startDate: "2022-05-01",
+            endDate: "2022-05-31",
+            image: "https://via.placeholder.com/150",
+            rating: 3.5,
+        },
+        {
+            id: 2,
+            title: "Announcement 2",
+            description: "This is the second announcement",
+            startDate: "2023-05-01",
+            endDate: "2023-05-31",
+            image: "https://via.placeholder.com/150",
+            rating: 4.9,
+        },
+        // add more announcements here
+    ];
     return (
         <Container>
             <NavbarHome />
+            <AnnouncementList announcements={announcements} />
             <Container className="fixed-bottom">
                 <FooterHome />
             </Container>
