45ca36679a8decc28c113bdc1c4b47210d771017
Bejan Andrei <bejan.andrei.official@gmail.com>
2023-05-12 18:47:17 +0300

    Diana implemented announcements page

--- client/src/AnnouncementCard.js
+++ client/src/AnnouncementCard.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Card, Col, Row } from "react-bootstrap";
 
 function AnnouncementCard(props) {
-    const { title, description, startDate, endDate, image, rating } = props;
+    const { title, description, startDate, endDate, image, rate } = props;
 
     return (
         <Card className="mb-4">
             <Row>
                 <Col md={4} className="text-center">
                     <Card.Img variant="top" src={image} />
-                    <Card.Text>Current rating: {rating}/5 &#x2605;</Card.Text>
+                    <Card.Text>Current rating: {rate}/5 &#x2605;</Card.Text>
                 </Col>
                 <Col md={8}>
                     <Card.Body>
--- client/src/Home.js
+++ client/src/Home.js
@@ -3,73 +3,39 @@ import Container from "react-bootstrap/Container";
 import React from "react";
 import FooterHome from "./FooterHome";
 import SearchBar from "./SearchBar";
+import { useEffect, useState } from "react";
+
 function Home() {
-    // fetch announcements from the server using the fetch API
-    let announcements = [];
+    const [announcements, setAnnouncements] = useState([]);
 
-    // await fetch('announcements', {
-    //     method: 'GET',
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'Content-Type': 'application/json'
-    //     },
-    // }).then((response) => {
-    //     if (response.status === 200) {
-    //         announcements.push(response.json());
-    //     } else {
-    //         console.log("Error");
-    //     }
-    // });
+    useEffect(() => {
+        // Fetch announcements from the server
+        fetch("announcements", {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => setAnnouncements(data))
+            .catch((error) => {
+                console.error("Error fetching announcements:", error);
+            });
+    }, []);
 
-    // const announcements = [
-    //     {
-    //         id: 1,
-    //         title: "Announcement 1",
-    //         description: "This is the first announcement",
-    //         startDate: "2022-05-01",
-    //         endDate: "2022-05-31",
-    //         image: "https://via.placeholder.com/150",
-    //         rating: 3.5,
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "Announcement 2",
-    //         description: "This is the second announcement",
-    //         startDate: "2023-05-01",
-    //         endDate: "2023-05-31",
-    //         image: "https://via.placeholder.com/150",
-    //         rating: 4.9,
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "Announcement 3",
-    //         description: "This is the third announcement",
-    //         startDate: "2023-05-01",
-    //         endDate: "2023-05-31",
-    //         image: "https://via.placeholder.com/150",
-    //         rating: 4.9,
-    //     },
-    //     {
-    //         id: 4,
-    //         title: "Announcement 4",
-    //         description: "This is the forth announcement",
-    //         startDate: "2023-05-01",
-    //         endDate: "2023-05-31",
-    //         image: "https://via.placeholder.com/150",
-    //         rating: 4.9,
-    //     },
-    //     // add more announcements here
-    // ];
     return (
         <Container>
-            <NavbarHome />
-            <SearchBar announcements={announcements} />
-
-            <Container className="fixed-bottom">
+            <Container className="top">
+                <NavbarHome />
+            </Container>
+            <Container className="mt-3">
+                <SearchBar announcements={announcements} />
+            </Container>
+            <Container className="bottom">
                 <FooterHome />
             </Container>
         </Container>
     );
 }
-
 export default Home;
