45d687aea64ff9c1d219c932dacc59a970279ece
DianaElenaStancu <stanciududa2002@gmail.com>
2023-06-04 17:25:12 +0200

    add logo and LandingPage.js page

--- client/src/generics/CustomNavbar.js
+++ client/src/generics/CustomNavbar.js
@@ -6,7 +6,17 @@ function CustomNavbar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand>Find your specialist</Navbar.Brand>
+                <Navbar.Brand>
+                    <img
+                        src="logo.svg"
+                        alt="Logo"
+                        width="40"
+                        height="40"
+                        className="d-inline-block align-top"
+                    />
+                    {' '}
+                    Find your specialist
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
